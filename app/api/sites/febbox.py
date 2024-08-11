7c27867767e83fdccbaacfbe4c2fe036ef22c1a5
Yogesh <yogeshkumarjamre1@gmail.com>
2024-08-11 07:24:00 +0530

    Fixing Febboxx Last

--- app/api/sites/febbox.py
+++ app/api/sites/febbox.py
@@ -47,7 +47,6 @@ def real_extract(url):
     base_url = f'{default_domain}share/{share_key}'
     session = requests.Session()
     initial_response = session.get(base_url).text
-    print(initial_response)
     soup = BeautifulSoup(initial_response, "html.parser")
     file_id_tag = soup.find("div", attrs={"class": "file"})
     if file_id_tag:
@@ -65,9 +64,7 @@ def real_extract(url):
         "share_key": share_key, 
     }
     
-    print(payload)
     initial_response = session.post("https://www.febbox.com/file/player", headers=initial_headers, data=payload, cookies=cookies, proxies=proxy).text
-    print(initial_response)
     
     pattern = r'var\s+sources\s*=\s*(\[.*?\]);'
     #print(initial_response)
